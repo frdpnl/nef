35d80bf4ba70432c84a7528a4afae88db4cf7089
frederic pinel <frederic.pinel@lxp.lu>
2025-05-22 17:34:00 +0200

    still designing...

--- src/monotone.c
+++ src/monotone.c
@@ -5,35 +5,38 @@
 #define VECSZ 64
 #endif
 
+// TODO typedef to distinguish the 2 vectors...
+
 typedef struct Vector_ {
     uint64_t n;  /* length of payload */
-    unsigned char typ[BLKSZ];
+    unsigned char typ[VECSZ];
     /* 
-     * -- typ[0] describes the raw array
-     *  0x00:
-     *  0x01: values, 'val
-     *  0x02: sub-points, 'sub
-     * -- Point.val:
-     * 0x00: empty (null)
-     * 0x01: next vector defined
-     * 0x00: uint64
-     * 0x01: int64
-     * 0x02: double
-     * 0x03: string
-     * -- Point.sub:
-     * 0x10: key (for 'sub)
-     * -- List of vectors:
-     * 0xff: chained vector index
+     * --- typ[0] describes the full raw array ---
+     *  0x00: empty
+     *  0x_1: type is values, 'val
+     *  0x_2: type is sub-points, 'sub
+     *  0x10: is there a chained vector
+     * --- typ[i] for values ---
+     *  0x00: empty (null)
+     *  0x01: uint64
+     *  0x02: int64
+     *  0x03: double
+     *  0x04: string location
+     * --- typ[i] for sub-keys ---
+     *  0x00: empty (null)
+     *  0x10: key location 
      */
-    uint64_t raw[BLKSZ];
+    uint64_t raw[VECSZ];
     /*
-     * raw[0] holds flags on the vector
+     * --- raw[0] next vector location ---
+     * --- raw[i] for values:  value of indicated type
+     * --- raw[i] for sub-keys: key location
      */
 } Vector;
 
 typedef struct Point_ {
     uint64_t key;
-    Vector val;
-    Vector sub;
+    Values val;
+    Subkeys sub;
 } Point;
 
