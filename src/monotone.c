b3a5d26e9629af3cee23ed8c28877aa3176424b9
frederic pinel <frederic.pinel@lxp.lu>
2025-05-22 09:58:26 +0200

    design exploration...

--- src/monotone.c
+++ src/monotone.c
@@ -1,16 +1,39 @@
 
 #include <stdint.h>
 
-#define BLKSZ 64
+#ifndef VECSZ
+#define VECSZ 64
+#endif
 
-typedef struct Block_ {
+typedef struct Vector_ {
+    uint64_t n;  /* length of payload */
     unsigned char typ[BLKSZ];
+    /* 
+     * -- typ[0] describes the raw array
+     *  0x00:
+     *  0x01: values, 'val
+     *  0x02: sub-points, 'sub
+     * -- Point.val:
+     * 0x00: empty (null)
+     * 0x01: next vector defined
+     * 0x00: uint64
+     * 0x01: int64
+     * 0x02: double
+     * 0x03: string
+     * -- Point.sub:
+     * 0x10: key (for 'sub)
+     * -- List of vectors:
+     * 0xff: chained vector index
+     */
     uint64_t raw[BLKSZ];
-} Block;
+    /*
+     * raw[0] holds flags on the vector
+     */
+} Vector;
 
 typedef struct Point_ {
     uint64_t key;
-    Block val;
-    Block sub;
+    Vector val;
+    Vector sub;
 } Point;
 
